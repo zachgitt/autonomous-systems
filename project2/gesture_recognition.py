dd9bf574c5e860f73e142d5595a0e8f9bb8e873a
Zachary Gittelman <zachgitt@umich.edu>
2020-02-20 23:33:10 -0500

    completed discretizing data through vector quantization, and determined optimal number of k

--- project2/gesture_recognition.py
+++ project2/gesture_recognition.py
@@ -3,6 +3,8 @@ from os.path import isfile, join
 import pandas as pd
 import seaborn as sns; sns.set()
 import matplotlib.pyplot as plt
+from scipy.spatial import cKDTree
+import numpy as np
 
 
 class Gesture:
@@ -12,6 +14,7 @@ class Gesture:
         self.df = df_in
         self.label = label_in
         self.file = file_in
+        self.codewords = []
 
     def get_df(self):
         return self.df
@@ -21,6 +24,10 @@ class Gesture:
 
     def get_file(self):
         return self.file
+
+    def set_codewords(self, cluster_indices):
+        assert(len(cluster_indices) == self.df.shape[0])
+        self.df['codewords'] = cluster_indices
 
 
 def file_to_jpg(file):
@@ -59,12 +66,17 @@ def get_label(file):
 
 # Plots the accelerometer and gyroscope data against the time data
 def plot_gestures(gestures, folder):
+
+    # Skip plotting
+    if listdir(folder):
+        return
+
     for gesture in gestures:
         df = gesture.get_df()
         plot = sns.lineplot(x='ts', y='value', hue='variable', data=pd.melt(df, ['ts']))
         fig = plot.get_figure()
         fig.savefig(folder + file_to_jpg(gesture.get_file()))
-        plt.show()
+        plt.clf()
 
 
 """
@@ -89,6 +101,113 @@ def load_txt_files(folder):
     return gestures
 
 
+# Return the center for k cluster means
+def kmeans(points, k):
+
+    # Assign k centers to random points
+    centers = points.sample(k)
+
+    # Iterate until centers unchanged
+    delta = 1
+    epsilon = 0.001
+    while delta > epsilon:
+        # Assign points to closest center
+        tree = cKDTree(centers)
+        _, indices = tree.query(points)
+
+        # Calculate cluster center as mean
+        clusters = points.assign(indices=indices)
+        new_centers = clusters.groupby(['indices']).mean()
+
+        # Update delta to converge
+        delta = np.linalg.norm(centers - new_centers.values)
+        centers = new_centers
+
+    return centers
+
+
+# Save the codeword (cluster) each gesture point belongs to
+def discretize(gestures, k):
+
+    # Get cluster centers
+    centers = kmeans(get_points(gestures), k)
+    tree = cKDTree(centers)
+
+    # Save codeword for each point (cluster)
+    for gesture in gestures:
+        _, indices = tree.query(gesture.get_df().drop(columns=['ts']))
+        gesture.set_codewords(indices)
+
+    return gestures
+
+
+def get_points(gestures):
+    # Concatenate all points
+    points = pd.DataFrame()
+    for gesture in gestures:
+        points = pd.concat([points, gesture.get_df().drop(columns=['ts'])], ignore_index=True)
+    return points
+
+
+"""
+Converts measurements into kmeans.
+"""
+def plot_kmeans(points, folder):
+
+    # Skip plotting
+    if listdir(folder):
+        return
+
+    # Init range [inclusive, exclusive)
+    k_start = 1
+    k_end = 101
+
+    # Try different k values, tracking variance
+    variances = []
+    for k in range(k_start, k_end):
+
+        # Track k centers that give lowest total variance
+        #best_centers = [None]*k
+        #min_var = math.inf
+
+        # Assign k centers to random points
+        centers = points.sample(k)
+
+        # Iterate until centers unchanged
+        delta = 1
+        epsilon = 0.001
+        variance = -1
+        while delta > epsilon:
+            # Assign points to closest center
+            tree = cKDTree(centers)
+            _, indices = tree.query(points)
+
+            # Calculate cluster center as mean
+            clusters = points.assign(indices=indices)
+            new_centers = clusters.groupby(['indices']).mean()
+
+            # Update delta to converge
+            delta = np.linalg.norm(centers - new_centers.values)
+            centers = new_centers
+
+            # Calculate variance
+            variance = clusters.groupby(['indices']).var().sum(axis=1).values[0]
+
+        variances.append(variance)
+
+        print("k=" + str(k) + " variance=" + str(variance))
+
+    # Plot k vs. reduction in variance (elbow method to find optimal k)
+    reductions = []
+    for v in variances:
+        reduction = variances[0] - v
+        reductions.append(reduction)
+    plt.plot(np.arange(k_start, k_end), reductions, '-o')
+    plt.savefig(folder + 'kmeans.jpg')
+
+
+
+
 def main():
 
     # Configuration
@@ -96,14 +215,20 @@ def main():
     train_multiple_folder = base + '/train_multiple/'
     train_single_folder = base + '/train_single/'
     plot_folder = base + '/imu_plots/'
+    kmeans_plot_folder = base + '/kmeans_plot/'
+    optimal_k = 10
 
-    # Load imu data
+    # Load train imu data
     gestures = []
     gestures += load_txt_files(train_multiple_folder)
     gestures += load_txt_files(train_single_folder)
 
     # Plot gestures
     plot_gestures(gestures, plot_folder)
+
+    # Discretize measurements with kmeans
+    plot_kmeans(get_points(gestures), kmeans_plot_folder)
+    gestures = discretize(gestures, optimal_k)
 
 
 if __name__ == '__main__':
