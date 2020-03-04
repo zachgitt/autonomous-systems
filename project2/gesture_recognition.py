completed initial implementation, but getting wrong values

project2/gesture_recognition.py
@@ -7,6 +7,18 @@ from scipy.spatial import cKDTree
 import numpy as np
 
 
+class Model:
+
+    def __init__(self, A_in, B_in, Pi_in, label_in):
+        self.A = A_in
+        self.B = B_in
+        self.Pi = Pi_in
+        self.label = label_in
+
+    def split_model(self):
+        return self.A, self.B, self.Pi
+
+
 class Gesture:
 
     # Define initializer
@@ -29,7 +41,7 @@ class Gesture:
         assert(len(cluster_indices) == self.df.shape[0])
         self.df['codewords'] = cluster_indices
 
-    def get_codwords(self):
+    def get_codewords(self):
         return self.df['codewords']
 
 
@@ -213,39 +225,138 @@ def get_class_codewords(gestures, label):
     codewords = []
     for gesture in gestures:
         if gesture.get_label() == label:
-            codewords.extend(gesture.get_codwords())
+            codewords.extend(gesture.get_codewords())
 
     return codewords
 
 
 def get_alpha(codewords, A, B, Pi):
 
-    import pdb; pdb.set_trace()
     # Initialization (t=0)
     T = len(codewords)
     N = len(Pi)
     alpha = np.ndarray(shape=(T, N))
     alpha[0] = Pi * B[codewords[0]]
 
-    # Induction (t=t+1)
+    # Save scales
+    scales = np.ndarray(shape=(T,))
+    scales[0] = np.sum(alpha[0])
+    alpha[0] /= scales[0]
+
+    # Induction (t+1=t)
     for t in range(1, T):
-        alpha[t] = np.dot(alpha[t-1], A.T) * B[codewords[1]]
+        alpha[t] = np.dot(alpha[t-1], A) * B[codewords[t]]
+        # Scale alpha
+        scales[t] = np.sum(alpha[t])
+        alpha[t] /= scales[t]
 
-    return alpha
-
-
-def emission_matrix():
-    return E
-
-
-def initial_probs():
-    return Pi
+    return alpha, scales
 
 
-def expectation_maximization(codewords, N, M):
+def get_beta(codewords, A, B, Pi):
+
+    # Initialize (T=end)
+    T = len(codewords)
+    N = len(Pi)
+    beta = np.ndarray(shape=(T, N))
+    beta[T-1] = np.ones(N)
+
+    # Save scales
+    scales = np.ndarray(shape=(T,))
+    scales[T-1] = np.sum(beta[T-1])
+    beta[T-1] /= scales[T-1]
+
+    # Induction
+    for t in reversed(range(T-1)):
+        beta[t] = np.dot(A, B[codewords[t+1]].T).T * beta[t+1]
+        # Scale beta
+        scales[t] = np.sum(beta[t])
+        beta[t] /= scales[t]
+
+    return beta, scales
+
+
+def get_gamma(alpha, beta):
+
+    # Initialize gamma
+    T, N = alpha.shape
+    gamma = np.ndarray(shape=(T, N))
+    gamma[T-1] = alpha[T-1]
+
+    # Iterate
+    for t in range(T-1):
+        gamma[t] = (alpha[t] * beta[t]) / np.dot(alpha[t], beta[t])
+
+    return gamma
+
+
+def get_xi(A, B, alpha, beta, codewords):
+
+    # Initialize xi
+    T, N = alpha.shape
+    xi = np.zeros(shape=(T, N, N))
+
+    # Iterate
+    for t in range(T-1):
+        for i in range(N):
+            for j in range(N):
+                xi[t][i][j] = alpha[t][i] * A[i][j] * B[codewords[t+1]][j] * beta[t+1][j]
+
+        # Normalize
+        norm = np.sum(xi[t])
+        xi[t] = xi[t] / norm
+
+    return xi
+
+
+def update_A(A, gamma, xi):
+
+    # Initialize lengths
+    N = A.shape[0]
+    T = gamma.shape[0]
+
+    for i in range(N):
+        for j in range(N):
+            top = 0
+            bottom = 0
+            for t in range(T):
+                top += xi[t][i][j]
+                bottom += gamma[t][i]
+            A[i][j] = top / bottom
+
+    return A
+
+
+def update_B(B, gamma, codewords):
+
+    # Initialize lengths
+    M, N = B.shape
+    T = gamma.shape[0]
+
+    for k in range(M):
+        for j in range(N):
+            top = 0
+            bottom = 0
+            for t in range(T):
+                if codewords[t] == k:
+                    top += gamma[t][j]
+                bottom += gamma[t][j]
+            B[k][j] = top / bottom
+
+    return B
+
+
+def likelihood_increased(likelihoods, delta=0.001):
+    if len(likelihoods) < 2:
+        return True
+
+    return delta < (likelihoods[-1] - likelihoods[-2])
+
+
+def expectation_maximization(codewords, N, M, label):
 
     # Initialize model (lambda)
-    model = None
+    T = len(codewords)
     A = np.full(shape=(N, N), fill_value=1/N)
     B = np.full(shape=(M, N), fill_value=1/N)
     Pi = np.full(shape=(N,), fill_value=0)
@@ -253,24 +364,43 @@ def expectation_maximization(codewords, N, M):
 
     # Stop when likelihood plateus
     likelihoods = []
-    likelihood_increases = True
-    while likelihood_increases:
+    while True:
 
         # Expectation step: calculate parameters
-        alpha = get_alpha(codewords, A, B, Pi)
-        beta = None
-        gamma = None
-        xi = None
+        alpha, alpha_scales = get_alpha(codewords, A, B, Pi)
+        beta, beta_scales = get_beta(codewords, A, B, Pi)
+        gamma = get_gamma(alpha, beta)
+        xi = get_xi(A, B, alpha, beta, codewords)
 
-        # Maximization step: improve T, E, Pi (lambda)
+        # Maximization step: improve A, B (lambda)
+        A = update_A(A, gamma, xi)
+        B = update_B(B, gamma, codewords)
 
         # Save likelihoods
-        likelihood = None
+        likelihood = np.sum(alpha[T-1])
         likelihoods.append(likelihood)
+        if not likelihood_increased(likelihoods, delta=0.001):
+            break
 
     # Plot likelihood values
 
+    # Return model
+    model = Model(A, B, Pi, label)
     return model
+
+
+def predict(gesture, models):
+
+    # Predict most probable model
+    predictions = np.zeros(shape=len(models))
+    for i, model in enumerate(models):
+        A, B, Pi = model.split_model()
+        codewords = gesture.get_codewords()
+        alpha, alpha_scales = get_alpha(codewords, A, B, Pi)
+        likelihood = np.sum(alpha[-1])
+        predictions[i] = likelihood
+
+    return predictions
 
 
 def main():
@@ -281,6 +411,8 @@ def main():
     train_single_folder = base + '/train_single/'
     plot_folder = base + '/imu_plots/'
     kmeans_plot_folder = base + '/kmeans_plot/'
+    test_folder = base + '/test/'
+    labels = ['beat3', 'beat4', 'circle', 'eight', 'inf', 'wave']
     optimal_k = 10
     num_hidden_states = 6
 
@@ -298,17 +430,25 @@ def main():
 
     # Model each type
     models = []
-    for label in ['beat3', 'beat4', 'circle', 'eight', 'inf', 'wave']:
+    for label in labels:
         codewords = get_class_codewords(gestures, label)
-        model = expectation_maximization(codewords, num_hidden_states, optimal_k)
+        model = expectation_maximization(codewords, num_hidden_states, optimal_k, label)
         models.append(model)
 
     # Load test imu data
+    test_gestures = load_txt_files(train_single_folder) # TODO: change to test folder
 
     # Discretize test data
+    test_gestures = discretize(test_gestures, optimal_k)
 
     # Predict test data (most probable model)
+    predictions = np.ndarray(shape=(len(test_gestures), len(models)))
+    for i, test in enumerate(test_gestures):
+        predictions[i] = predict(test, models)
 
+    # Print predictions
+    df = pd.DataFrame(data=predictions, columns=labels)
+    print(df)
 
 if __name__ == '__main__':
     main()